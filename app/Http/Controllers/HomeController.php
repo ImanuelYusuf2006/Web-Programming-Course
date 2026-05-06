c26dcba23a12be048d51b381253579435ca26315
Cupapimumenyo <yusuf.setiobudi2006@gmail.com>
2026-05-06 14:02:14 +0700

    tidy up the data on controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,9 +8,18 @@ class HomeController extends Controller
 {
     public function index() {
         $students = [
-            ['id' => 1, 'name' => 'Tony Stark', 'score' => [97, 95, 90]],
-            ['id' => 2, 'name' => 'Jane Smith', 'score' => [86, 76, 90]],
-            ['id' => 3, 'name' => 'Michael Johnson','score' => [55, 64, 67]],
+            ['id' => 1, 
+            'name' => 'Tony Stark', 
+            'score' => [97, 95, 90]
+            ],
+            ['id' => 2, 
+            'name' => 'Jane Smith',
+            'score' => [86, 76, 90]
+            ],
+            ['id' => 3,
+            'name' => 'Michael Johnson',
+            'score' => [55, 64, 67]
+            ],
         ];
         return view('home', compact('students'));
     }
