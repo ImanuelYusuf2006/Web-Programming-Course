cb7297ecb5e53f1067a060064197ffd5bc814f21
Cupapimumenyo <yusuf.setiobudi2006@gmail.com>
2026-05-13 15:02:56 +0700

    fix : clean the unused part

--- app/Models/Students.php
+++ app/Models/Students.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Students extends Model
 {
@@ -20,7 +19,7 @@ class Students extends Model
 
     public function getAverage() : float {
         if($this->relationLoaded('student_scores')) {
-            $count = $this->scores->count();
+            $count = $this->student_scores->count();
             if($count == 0){
                 return 0;
             }
